a9f58f210e2bc2120197e3f85d4c9a415edb3e02
peachykish <nakisha.carroll@gmail.com>
2020-08-06 14:57:28 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,32 +1,41 @@
-
 let todos = [
-    {
-        completed: false,
-        description: "Take Jim to the hair salon"
-    },
-    {
-        completed: true,
-        description: "Drop off wedding invitation at mailbox"
-    },
-    {
-        completed: false,
-        description: "Pick up the cake"
-    },
-    {
-        completed: false,
-        description: "Call the caterers"
-    }
+  {
+    completed: false,
+    description: "Take Jim to the hair salon",
+  },
+  {
+    completed: true,
+    description: "Drop off wedding invitation",
+  },
+  {
+      completed: false,
+      description: "Pick up the cake"
+  },
+  {
+      completed: false,
+      description: "Call the caterers"
+  }
 ];
 
 function renderTodoApp() {
-    let app = document.querySelector("#app")
-    let h1 = document.createElement("h1")
-    // your code here
+  let app = document.querySelector("#app");
+  let h1 = document.createElement("h1");
+  let ul = document.createElement("ul");
+  ul.style = "list-style-type: none";
 
-    h1.innerText = "Todo List"
+  for (let i = 0; i < todos.length; i++) {
+    let li = document.createElement("li");
+    let input = `<input type="checkbox">${todos[i].description}</input>`;
+    if (todos[i].checked === true) {
+      input = `<input type="checkbox" checked>${todos[i].description}</input>`;
+    }
+    ul.appendChild(li);
+    li.innerHTML += input;
+  }
 
-    app.appendChild(h1)
-    // and maybe some here
+  h1.innerText = "todoList";
+  app.appendChild(h1);
+  app.appendChild(ul);
 }
 
-renderTodoApp()
+renderTodoApp();
